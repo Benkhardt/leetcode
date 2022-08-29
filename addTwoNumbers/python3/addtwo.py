a9445c10a90b56004e19d445c3ab4fd119740ca9
dbenkhar <benkhardt@msn.com>
2022-08-29 15:37:17 +0200

    added version with less variables

--- addTwoNumbers/python3/addtwo.py
+++ addTwoNumbers/python3/addtwo.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    addtwo.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: dbenkhar <dbenkhar@students.42wolfsburg.de +#+  +:+       +#+         #
+#    By: dbenkhar <dbenkhardt@students.42wolfsburg. +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/29 06:49:05 by dbenkhar          #+#    #+#              #
-#    Updated: 2022/08/29 06:50:07 by dbenkhar         ###   ########.fr        #
+#    Updated: 2022/08/29 15:27:33 by dbenkhar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,42 +20,39 @@ class Solution:
     def addTwoNumbers(self, l1, l2):
         hold = 0
         new_lst = None
-        tmp1 = l1
-        tmp2 = l2
-        while tmp1 and tmp2 is not None:
-            math = tmp1.val + tmp2.val + hold
-            hold = 0
-            if math > 9:
-                math = math % 10
-                hold = 1
+        #tmp1 = l1
+        #tmp2 = l2
+        while l1 and l2 is not None:
             new_node = ListNode()
-            new_node.val = math
             new_node.next = new_lst
-            new_lst = new_node
-            tmp1 = tmp1.next
-            tmp2 = tmp2.next
-        while tmp1 is not None:
-            new_node = ListNode()
-            math = tmp1.val + hold
+            new_node.val = l1.val + l2.val + hold
             hold = 0
-            if math > 9:
-                math = math % 10
+            if new_node.val > 9:
+                new_node.val = new_node.val % 10
                 hold = 1
-            new_node.val = math
-            new_node.next = new_lst
             new_lst = new_node
-            tmp1 = tmp1.next
-        while tmp2 is not None:
+            l1 = l1.next
+            l2 = l2.next
+        while l1 is not None:
             new_node = ListNode()
-            math = tmp2.val + hold
-            hold = 0
-            if math > 9:
-                math = math % 10
-                hold = 1
-            new_node.val = math
             new_node.next = new_lst
+            new_node.val = l1.val + hold
+            hold = 0
+            if new_node.val > 9:
+                new_node.val = new_node.val % 10
+                hold = 1
             new_lst = new_node
-            tmp2 = tmp2.next
+            l1 = l1.next
+        while l2 is not None:
+            new_node = ListNode()
+            new_node.next = new_lst
+            new_node.val = l2.val + hold
+            hold = 0
+            if new_node.val > 9:
+                new_node.val = new_node.val % 10
+                hold = 1
+            new_lst = new_node
+            l2 = l2.next
         if hold != 0:
             new_node = ListNode()
             new_node.val += hold
