4607a2595679252ebac795cf1ab54bbebc010cb3
Dennis Benkhardt <dbenkhardt@icloud.com>
2022-09-12 15:05:30 +0200

    update

--- mergeTwoSortedLists/cpp/dev/main.cpp
+++ mergeTwoSortedLists/cpp/dev/main.cpp
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   merge.cpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbenkhar <dbenkhardt@student.42wolfsburg.d +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/11 20:36:51 by dbenkhar          #+#    #+#             */
-/*   Updated: 2022/09/11 20:53:04 by dbenkhar         ###   ########.fr       */
+/*   Created: 2022/09/11 20:53:29 by dbenkhar          #+#    #+#             */
+/*   Updated: 2022/09/12 15:05:18 by dbenkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "merge.hpp"
 
-t_ListNode *Solution::mergeTwoLists(t_ListNode *list1, t_ListNode *list2){
-	
+int main(){
+
 }
