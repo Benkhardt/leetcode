72777f9782ca5f3bfbf33aadc4846e7f38c46085
Dennis Benkhardt <dbenkhardt@icloud.com>
2022-09-12 22:12:57 +0200

    dev seems fine with own tests

--- mergeTwoSortedLists/c/dev/main.c
+++ mergeTwoSortedLists/c/dev/main.c
@@ -6,7 +6,7 @@
 /*   By: dbenkhar <dbenkhardt@student.42wolfsburg.d +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 15:48:02 by dbenkhar          #+#    #+#             */
-/*   Updated: 2022/09/12 21:58:34 by dbenkhar         ###   ########.fr       */
+/*   Updated: 2022/09/12 22:10:19 by dbenkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,15 @@ int main(int argc, char **argv){
 	}
 	t_ListNode	*l1 = createList(argv[1]);
 	t_ListNode	*l2 = createList(argv[2]);
+	t_ListNode	*l3;
+
+	printf("before merge...\n");
 	printList(l1);
 	printList(l2);
-	l2 = reverseList(l2);
+	l3 = mergeTwoLists(l1, l2);
+	printf("after merge...\n");
+	printList(l1);
 	printList(l2);
+	printList(l3);
 	return 0;
 }
--- mergeTwoSortedLists/c/dev/merge.c
+++ mergeTwoSortedLists/c/dev/merge.c
@@ -6,7 +6,7 @@
 /*   By: dbenkhar <dbenkhardt@student.42wolfsburg.d +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 15:48:00 by dbenkhar          #+#    #+#             */
-/*   Updated: 2022/09/12 21:35:08 by dbenkhar         ###   ########.fr       */
+/*   Updated: 2022/09/12 22:10:47 by dbenkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,38 @@
 struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2){
 	if (!list1 || !list2)
 		return (NULL);
-	struct ListNode	list;
+	struct ListNode	*rtn, *new;
+	rtn = NULL;
 
-	list.next = NULL;
-	list.val = 0;
-	
+	while (list1 != NULL && list2 != NULL){
+		new = malloc(sizeof(t_ListNode));
+		if (list1->val < list2->val){
+			new->val = list1->val;
+			new->next = rtn;
+			rtn = new;
+			list1 = list1->next;
+		}
+		else if (list1->val >= list2->val){
+			new->val = list2->val;
+			new->next = rtn;
+			rtn = new;
+			list2 = list2->next;
+		}
+	}
+	while (list1 != NULL){
+		new = malloc(sizeof(t_ListNode));
+		new->val = list1->val;
+		new->next = rtn;
+		rtn = new;
+		list1 = list1->next;
+	}
+	while (list2 != NULL){
+		new = malloc(sizeof(t_ListNode));
+		new->val = list2->val;
+		new->next = rtn;
+		rtn = new;
+		list2 = list2->next;
+	}
+	rtn = reverseList(rtn);
+	return (rtn);
 }
